f8004a68bbf4eda6f894908cb990ee806cd46b94
Stanislav Khrapov <khrapovs@gmail.com>
2015-07-06 13:26:08 +0300

    Write setup file

---
+++ README.rst
@@ -0,0 +1,25 @@
+.. image:: https://readthedocs.org/projects/famamcbeth/badge/?version=latest
+	:target: https://readthedocs.org/projects/famamcbeth/?badge=latest
+	:alt: Documentation Status
+
+Fama-McBeth regressions
+=======================
+
+Estimate risk premia in Fama-McBeth regressions with both 2-step OLS and GMM.
+
+Documentation
+-------------
+
+`Documentation <http://famamcbeth.readthedocs.org/en/latest/>`_
+
+Requirements
+------------
+
+- `mygmm <https://github.com/khrapovs/mygmm>`_
+
+Installation
+------------
+
+::
+
+	pip install git+git://github.com/khrapovs/famamcbeth
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, Extension, find_packages
+from setuptools.dist import Distribution
+
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='famamcbeth',
+      version='1.0',
+      description=('Estimate risk premia in Fama-McBeth regressions with both 2-step OLS and GMM'),
+      long_description=long_description,
+      author='Stanislav Khrapov',
+      license='MIT',
+      author_email='khrapovs@gmail.com',
+      url='https://github.com/khrapovs/argamma',
+      py_modules=['famamcbeth'],
+      packages=find_packages(),
+      keywords=['risk', 'return', 'premium', 'Fama', 'McBeth', 'regression',
+        'OLS', 'GMM', 'estimation'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+      ],
+)
